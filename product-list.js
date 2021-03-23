7e012b20bbcded2c2baace23ff69504c1f98e876
Marcus Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-03-22 22:14:43 -0400

    Update product-list.js
    I replaced my old code with the modified and condensed code. We combined several pages and shortened our code for more efficiency. Bootstrap was also implemented for product cards

--- product-list.js
+++ product-list.js
@@ -1,68 +1,72 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card } from 'react-bootstrap';
-
-// class Products extends Component {
-//     constructor(props) {
-//         super(props)
-//     }
-
-//     render() {
-//         return (
-//             <div className="productCards">
-//                 <img className="productImage" src={this.props.item.image} />
-//                 <h3 className="productHeaders">{this.props.item.name} - {this.props.item.price}</h3>
-//                 <p>{this.props.item.description}</p>
-//             </div>
-//         )
-//     }
-// }
 
 const ProductsInfo = () => {
     const cardInfo = [
         {
             id: "1",
             name: "Lion",
-            price: "$1,000",
+            price: "$6,000",
             description: "",
             image: "https://cdn.britannica.com/29/150929-050-547070A1/lion-Kenya-Masai-Mara-National-Reserve.jpg"
         },
         {
             id: "2",
             name: "Tiger",
-            price: "$3,000",
+            price: "$6,500",
             description: "",
             image: "https://upload.wikimedia.org/wikipedia/commons/8/81/2012_Suedchinesischer_Tiger.JPG"
         },
         {
             id: "3",
-            name: "lion",
+            name: "Snake",
             price: "$1,000",
             description: "",
             image: "https://images.unsplash.com/photo-1571391733814-15ac29ac3544?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80"
         },
         {
             id: "4",
-            name: "lion",
-            price: "1,000",
+            name: "Zebra",
+            price: "$2,500",
             description: "",
-            image: ""
+            image: "https://images.unsplash.com/photo-1509870796364-6959d105b09c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
         },
         {
             id: "5",
-            name: "lion",
-            price: "$1,000",
+            name: "Octopus",
+            price: "$8,000",
             description: "",
-            image: ""
+            image: "https://images.unsplash.com/photo-1545671913-b89ac1b4ac10?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
         },
         {
             id: "6",
-            name: "lion",
+            name: "Jellyfish",
             price: "$1,000",
             description: "",
-            image: ""
+            image: "https://images.unsplash.com/photo-1501561942-ec1065e470aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
+
         },
     ];
+
+    const renderCard = (card, index) => {
+        return (
+            <div className="cards">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={card.image} />
+                    <Card.Body>
+                        <Card.Title>{card.name} - {card.price}</Card.Title>
+                        <Card.Text>
+                            {card.description}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    };
+    return <div className="grid">{cardInfo.map(renderCard)}</div>;
+};
+export default ProductsInfo;
 
 const renderCard = (card, index) => {
     return (
