6479aa105bab11a2e127f8a648695b347b67f855
Marcus Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-03-23 16:14:29 -0400

    Update about.js

--- about.js
+++ about.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card } from 'react-bootstrap';
 
 const About = () => {
     return (
         <div>
             <h1>This is About Page</h1>
+            <div className="aboutpage-cards">
+                <Card className="aboutpage-card" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            lorem ljdhf lkjsadh flksajd hf;lajdh f;s
+                    </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card className="aboutpage-card" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            lorem ljdhf lkjsadh flksajd hf;lajdh f;s
+                    </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     )
 }
