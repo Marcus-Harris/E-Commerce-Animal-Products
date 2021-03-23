36e2333f8a61e70daee3b7a22de0a486183f4a4b
Marcus Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-03-23 14:43:16 -0400

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="FooterPage">
+            <p>2021 © copyright</p>
+        </footer>
+    )
+}
+
+export default Footer;
