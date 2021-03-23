a2781215f10b58867515fa4a49bf68b16c4bc64f
Shweta Deshmukh <79119065+shwetadskh@users.noreply.github.com>
2021-03-23 13:22:03 -0400

    Update product-list.js

--- product-list.js
+++ product-list.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React , {useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card } from 'react-bootstrap';
+// this is required for flip
+import ReactCardFlip from 'react-card-flip';
+
 const ProductsInfo = (i) => {
+   
     const cardInfo = [
         {
             id: "1",
@@ -15,7 +19,7 @@ const ProductsInfo = (i) => {
             id: "2",
             name: "Tiger",
             price: "$6,500",
-            description: "The nine subspecies of Tigers are restricted to small pockets of Asia.",
+            description: "",
             image: "https://upload.wikimedia.org/wikipedia/commons/8/81/2012_Suedchinesischer_Tiger.JPG",
             show: false
         },
@@ -23,7 +27,7 @@ const ProductsInfo = (i) => {
             id: "3",
             name: "Snake",
             price: "$1,000",
-            description: " Snakes live in almost every corner of the world. They are found in forests, deserts, swamps and grasslands.They have no limbs, no moveable eyelids, and no ear openings.",
+            description: "This is snake",
             image: "https://images.unsplash.com/photo-1571391733814-15ac29ac3544?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
             show: false
         },
@@ -31,7 +35,7 @@ const ProductsInfo = (i) => {
             id: "4",
             name: "Zebra",
             price: "$2,500",
-            description: "Three species of zebras live in Africa. They also have great hearing and an excellent sense of taste and smell",
+            description: "",
             image: "https://images.unsplash.com/photo-1509870796364-6959d105b09c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
             show: false
         },
@@ -39,7 +43,7 @@ const ProductsInfo = (i) => {
             id: "5",
             name: "Octopus",
             price: "$8,000",
-            description: "The octopus can only be found in salt water, but they live in all the oceans. They are considered the most intelligent of all the invertebrates",
+            description: "",
             image: "https://images.unsplash.com/photo-1545671913-b89ac1b4ac10?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
             show: false
         },
@@ -47,42 +51,68 @@ const ProductsInfo = (i) => {
             id: "6",
             name: "Jellyfish",
             price: "$1,000",
-            description: "These creatures have global distribution. You can find them in every ocean on Earth",
+            description: "",
             image: "https://images.unsplash.com/photo-1501561942-ec1065e470aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
             show: false
+
         },
     ];
-    const MouseIn = (e) => {
-        console.log("++++++ Inside hover: " + i) ;
-        //e.target.style.background = 'red';
-        e.target.style.display= 'none';
-        //return cardInfo ;
+// isFlipped array to store flip status for each card,every card is display-false
+    const [ isFlipped,setFlip] = useState([false, false, false, false, false, false]) ;
+
+    const handleClickFlip = (i) => {
+        
+        //setFlip = (i) =>{isFlipped[i] = true} ;
+        let newArr = [...isFlipped];
+        newArr[i] = true ;
+        setFlip(newArr) ;
+        console.log("+++++ " + i)
+        
     }
-    const MouseOut = (e) => {
-        console.log("++++++ Inside hover: " ) ;
-        //e.target.style.background = 'white';
-        e.target.style.display= 'block';
-        //return cardInfo ;
+
+    const handleClickUnFlip = (i) => {
+       
+        //setFlip(false) ;
+        let newArr = [...isFlipped];
+        newArr[i] = false ;
+        setFlip(newArr) ;
+        
     }
-       const renderCard = (card, index) => {
+
+   
+    const renderCard = (card, index) => {
         return (
-            <div className="cards">
-                <Card  style={{ width: '18rem' }}   >
-                    <Card.Img  onMouseOver={MouseIn} onMouseOut={MouseOut} variant="top" src={card.image} />
-                    <Card.Body >
-                        <Card.Title style = {{display:'block'}}  >{card.name} - {card.price}</Card.Title>
-                        <Card.Text>
-                            {card.description}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <footer className="FooterPage">
-                    <p>2021 © copyright </p>
-                </footer>
-            </div>
+            // isFlipped is a parameter which will decide which div to be display 
+            <ReactCardFlip  isFlipped={isFlipped[index]} flipDirection="horizontal">
+            
+                {/* div without flip */}
+                <div className="cards">
+                    <Card  style={{ width: '18rem' }}   >
+                    <Card.Img  variant="top" src={card.image} />   
+                        
+                    </Card>
+                    <button onClick={() => handleClickFlip(index)}>Click to flip</button>
+                </div>
+               
+                 {/* div with unflip */}
+               
+                <div className="cards">
+                    <Card  style={{ width: '18rem' }}   >                    
+                        <Card.Body >                        
+                            <Card.Title style = {{display:'block'}}  >{card.name} - {card.price}</Card.Title>
+                            
+                            <Card.Text>
+                                {card.description}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <button onClick={() => handleClickUnFlip(index)}>Click to Unflip</button>       
+                </div>
+                
+             </ReactCardFlip>
         );
     };
-
     return <div className="grid">{cardInfo.map(renderCard)}</div>;
 };
+
 export default ProductsInfo;
